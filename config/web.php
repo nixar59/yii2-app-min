989a8ba635808e7173172964a82937a8a13313e3
Nixar <bushmakin59@gmail.com>
2019-02-04 23:15:41 +0500

    Initial commit

---
+++ config/web.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'id' => 'yii2-app-min',
+    'basePath' => dirname(__DIR__),
+    'components' => [
+        'request' => [
+            // !!! IMPORTANT !!!
+            // Uncomment following lines and replace secret key for cookie validation
+            /*
+            'cookieValidationKey' => 'theSecretKeyHere'
+            */
+        ],
+    ],
+];
---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bushm
+ * Date: 04.02.2019
+ * Time: 22:50
+ */
+
+/* @var $this \yii\web\View */
+
+echo "<h1>Skibidi wa-pa-pa!</h1>";
